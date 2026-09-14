27073bcb343db85d8f49ace574e5d90466f327d6
r2agent <r2agent06@gmail.com>
2026-09-14 12:31:54 +0800

    Update PhotoboothSession.tsx

--- src/PhotoboothSession.tsx
+++ src/PhotoboothSession.tsx
@@ -505,7 +505,9 @@ export const PhotoboothSession = ({ event, cameraSettings, allSettings, onExit }
         }
         if (rawSlotPhotos.length > 0) {
           try {
-            slideshowVideo = await generateSlideshowVideo(rawSlotPhotos, 500);
+            const TOTAL_VIDEO_DURATION_MS = 3000; // <- ubah angka ini buat atur total durasi video (dalam milidetik)
+            const perPhotoMs = TOTAL_VIDEO_DURATION_MS / rawSlotPhotos.length;
+            slideshowVideo = await generateSlideshowVideo(rawSlotPhotos, perPhotoMs);
           } catch {
             slideshowVideo = null;
           }
